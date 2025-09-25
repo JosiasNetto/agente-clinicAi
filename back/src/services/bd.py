d4137e269f058832b9872404ec4b86709993e042
JosiasNetto <jhcndev@gmail.com>
2025-09-24 22:16:42 -0300

    feat: Adiciona função para criar conversas

---
+++ back/src/services/bd.py
@@ -0,0 +1,7 @@
+from src.config.db import db
+import uuid
+
+async def create_conversation():
+    conversation_id = str(uuid.uuid4())
+    await db.conversations.insert_one({"session_id": conversation_id, "messages": []})
+    return conversation_id
