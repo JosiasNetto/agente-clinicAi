eeb5ec020ddd5d6de629a5a376b6adfc58eb0ed3
JosiasNetto <jhcndev@gmail.com>
2025-09-24 22:18:58 -0300

    feat: Atualiza função de criação de conversas para incluir triagem

--- back/src/services/bd.py
+++ back/src/services/bd.py
@@ -3,5 +3,5 @@ import uuid
 
 async def create_conversation():
     conversation_id = str(uuid.uuid4())
-    await db.conversations.insert_one({"session_id": conversation_id, "messages": []})
+    await db.conversations.insert_one({"session_id": conversation_id, "mensagens": [], "triagem": {}})
     return conversation_id
