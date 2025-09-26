a456364ffd854dc1b5fcc2762b169092cb98056c
JosiasNetto <jhcndev@gmail.com>
2025-09-25 22:37:59 -0300

    feat: Atualiza a configuração do cliente Gemini para usar a chave da API e validação de ambiente

--- back/src/config/gemini.py
+++ back/src/config/gemini.py
@@ -1,9 +1,13 @@
-from google import genai
+import google.generativeai as genai
 import os
+from dotenv import load_dotenv
 
-def get_gemini_client():
-    API_KEY = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=API_KEY)
-    return client
+load_dotenv()
 
-client = get_gemini_client()
+API_KEY = os.environ.get("GEMINI_API_KEY")
+if not API_KEY:
+    raise ValueError("GEMINI_API_KEY não está definido nas variáveis de ambiente")
+
+genai.configure(api_key=API_KEY)
+
+client = genai
