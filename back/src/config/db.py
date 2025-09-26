cdcf743022d9a7d1a9dd0993a5191f0cdb823d3e
JosiasNetto <jhcndev@gmail.com>
2025-09-25 22:37:47 -0300

    feat: Atualiza a conexão com o MongoDB para usar o banco de dados "chatbot-clinica"

--- back/src/config/db.py
+++ back/src/config/db.py
@@ -1,5 +1,8 @@
 import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def connect_to_mongo():
     MONGODB_URL = os.getenv("MONGODB_URL")
@@ -8,7 +11,7 @@ def connect_to_mongo():
         raise ValueError("MONGODB_URL não está definido nas variáveis de ambiente")
 
     client = MongoClient(MONGODB_URL, serverSelectionTimeoutMS=5000)
-    db = client.get_database()
+    db = client.get_database("chatbot-clinica")
 
     return db
 
