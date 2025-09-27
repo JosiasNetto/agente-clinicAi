c875f68b36d7bd5bfa3e66e82fee4ffa0006d301
Josias <jhcndev@gmail.com>
2025-09-27 12:34:58 -0300

    feat: Adiciona configuração de CORS no arquivo main.py

--- back/main.py
+++ back/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from  src.routes.chatRoutes import router as chat_router
 
 app = FastAPI()
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 
 app.include_router(chat_router, prefix="/chat", tags=["chat"])
 
