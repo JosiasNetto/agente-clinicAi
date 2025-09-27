a5b22165d8a5b4a68ecb7eafb1716727a80f6f92
Josias <jhcndev@gmail.com>
2025-09-27 12:47:33 -0300

    feat: Adiciona endpoint para iniciar uma nova conversa na rota de chat

--- back/src/routes/chatRoutes.py
+++ back/src/routes/chatRoutes.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-from src.controllers.chatController import post_message
-from src.models.conversaModels import MessageRequest
+from src.controllers.chatController import (
+    post_conversation, post_message
+)
+from src.models.conversaModels import (
+    ConversationRequest, MessageRequest
+)
 
 router = APIRouter()
+
+@router.post("/")
+async def a_post_conversation(request: ConversationRequest):
+    return await post_conversation(numero_paciente=request.numero_paciente)
 
 @router.post("/message")
 async def a_post_message(request: MessageRequest):
