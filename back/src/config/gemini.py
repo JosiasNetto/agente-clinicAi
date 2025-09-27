b8db9bf80a5a4129105254fa6a8117c1889e5369
Josias <jhcndev@gmail.com>
2025-09-27 12:01:35 -0300

    feat: Atualiza a configuração da chave da API para usar IA_API_KEY

--- back/src/config/gemini.py
+++ back/src/config/gemini.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.environ.get("GEMINI_API_KEY")
-if not API_KEY:
-    raise ValueError("GEMINI_API_KEY não está definido nas variáveis de ambiente")
+IA_API_KEY = os.environ.get("IA_API_KEY")
+if not IA_API_KEY:
+    raise ValueError("IA_API_KEY não está definido nas variáveis de ambiente")
 
-genai.configure(api_key=API_KEY)
+genai.configure(api_key=IA_API_KEY)
 
 client = genai
