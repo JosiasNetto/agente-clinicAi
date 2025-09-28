defb49191ee97717cf9d82763f595bcf584f6bae
Josias <jhcndev@gmail.com>
2025-09-28 13:07:11 -0300

    refactor: Renomeia funções para incluir sufixo '_bd' e melhorar a clareza

--- back/src/services/bd.py
+++ back/src/services/bd.py
@@ -5,7 +5,7 @@ from src.models.conversaModels import (
     Mensagem
 )
 
-def create_conversation(numero_paciente: str = None):
+def create_conversation_bd(numero_paciente: str = None):
     session_id = str(uuid.uuid4())
     conversa = Conversa(session_id=session_id, mensagens=[], triagem={}, numero_paciente=numero_paciente)
     db.conversas.insert_one(conversa.dict())
@@ -17,13 +17,13 @@ def get_conversation_messages_bd(session_id: str):
         return Conversa(**conversation).mensagens
     return None
 
-def get_conversations_by_number(numero_paciente: str):
+def get_conversations_by_number_bd(numero_paciente: str):
     conversations = db.conversas.find({"numero_paciente": numero_paciente})
     return [Conversa(**conv).dict() for conv in conversations]
 
-def update_conversation(session_id: str, mensagem: dict):
+def update_conversation_bd(session_id: str, mensagem: dict):
     mensagem = Mensagem(**mensagem)
     db.conversas.update_one({"session_id": session_id}, {"$push": {"mensagens": mensagem.dict()}})
 
-def update_triage(session_id: str, triage_data: dict):
+def update_triage_bd(session_id: str, triage_data: dict):
     db.conversas.update_one({"session_id": session_id}, {"$set": {"triagem": triage_data}})
